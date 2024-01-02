f2c27361fdc8fe948ca50df0c7538bd76fa02882
Sidiki239 <dembelesidiki789@gmail.com>
2024-01-02 17:00:09 +0100

    my portfolio website

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import {
+  AiFillTwitterCircle,
+  AiFillGithub,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaFacebook, FaLinkedinIn } from "react-icons/fa";
+import { FaTelegramPlane } from "react-icons/fa";
+import { IoLogoWhatsapp } from "react-icons/io";
+import { RiInstagramFill } from "react-icons/ri";
+import { BsPerson } from "react-icons/bs";
+import { FaMailchimp } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa";
+import { MdCopyright } from "react-icons/md";
+const Footer = () => {
+  return (
+    <div id="works" className=" mx-auto m-auto h-[100px]  mt-16 sm:h-[100px]">
+      <div className=" bg-yellow-400 h-full flex flex-col   justify-center mt-17 ">
+   
+      <h3  style={{marginTop:"20px", justifyContent:"center", display:"flex"}} > <> <b>SIDIKI DEMBÉLÉ</b>  &nbsp; <MdCopyright />  &nbsp; Tous droits réservés   </> 
+      </h3>
+     
+      
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/data/data.js
@@ -0,0 +1,28 @@
+
+
+export const data = [
+  {
+    id: 1,
+    img: 'https://i.postimg.cc/GtjQ9NnD/Capture-d-cran-du-2024-01-02-16-41-00.png',
+    title: "Blog Website",
+    desc: "",
+    link: "",
+    git: "",
+  },
+  {
+    id: 2,
+    img: 'https://i.postimg.cc/9Q5ByKhs/Capture-d-cran-du-2024-01-02-16-49-19.png',
+    title: "Transport Website",
+    link: "https://demtransport.netlify.app/",
+    git: "",
+  },
+  {
+    id: 3,
+    img: 'https://i.postimg.cc/52QK5LTk/Capture-d-cran-du-2024-01-02-16-54-26.png',
+    title: "E-commerce",
+    desc: "",
+    link: "",
+    git: "",
+  }
+  
+];
